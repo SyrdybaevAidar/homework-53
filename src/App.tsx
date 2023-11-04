b701785d32ff1d11e8380d7b9e91dccb55a9de64
aidar_syrdybaev <ajdarsyrdybaev@gmail.com>
2023-11-05 01:54:30 +0600

    add task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
+import { TaskModel } from './Task';
+import { Task } from './Task';
 import './App.css'
 
  const App = () => {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    new TaskModel("Сделать уборку"),
+    new TaskModel("Прогулка"),
+    new TaskModel("Работа"),
+    new TaskModel("Тренировка")
+  ]);
+
+  const removeAction = (id: string) => {
+    const copyTasks = tasks.filter(it => it.Id != id);
+    setTasks(copyTasks);
+  };
+
+  const taskComponents = tasks.map(item => 
+    (<Task Task={item} RemoveAction={() => removeAction(item.Id)}></Task>)  
+  );
   return (
     <>
-
+      {taskComponents}
     </>
   )
 };
--- src/task.tsx
+++ src/task.tsx
@@ -1,10 +1,38 @@
 import React  from "react";
 
-interface ITaskProps extends React.PropsWithChildren{
+const generateUUID = () => {
+    let d = new Date().getTime();
+    let d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      let r = Math.random() * 16;
+      if (d > 0) {
+        r = (d + r) % 16 | 0;
+        d = Math.floor(d / 16);
+      } else {
+        r = (d2 + r) % 16 | 0;
+        d2 = Math.floor(d2 / 16);
+      }
+      return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+    });
+  };
+  
+
+export class TaskModel {
+    Id: string;
     Name: string;
+    constructor(name: string){
+        this.Id = generateUUID();
+        this.Name = name;
+    }
 }
+
+interface ITaskProps extends React.PropsWithChildren{
+    Task: TaskModel;
+    RemoveAction(id: string): void;
+}
+
 export const Task = (props: ITaskProps) => {
     return <div>
-        <p>{props.Name}</p><button>x</button>
+        <p>{props.Task.Name}</p><button onClick={() => props.RemoveAction(props.Task.Id)}>x</button>
     </div>
 };
